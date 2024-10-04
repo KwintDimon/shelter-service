9e313060b0200f090af05722c0e8d2e178d857ff
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2024-10-05 00:15:45 +0300

    implemented shelter filtration

--- backend/shelter/admin.py
+++ backend/shelter/admin.py
@@ -5,6 +5,21 @@ from shelter.models import (
     Shelter,
 )
 
-admin.site.register(ShelterType)
-admin.site.register(Shelter)
-admin.site.register(Facility)
+
+@admin.register(Shelter)
+class ShelterAdmin(admin.ModelAdmin):
+    list_display = ("shelter_type", "user", "location", "capacity", "created_at")
+    list_filter = ("shelter_type", "location")
+    search_fields = ("location", "description")
+
+
+@admin.register(Facility)
+class FacilityAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(ShelterType)
+class ShelterTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
--- backend/shelter/views.py
+++ backend/shelter/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, generics
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework import viewsets, generics, serializers
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
 
 from shelter.models import (
@@ -23,6 +25,57 @@ class ShelterViewSet(viewsets.ModelViewSet):
         else:
             self.permission_classes = [IsAuthenticated]
         return super().get_permissions()
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        try:
+            return [int(str_id) for str_id in qs.split(",")]
+        except ValueError:
+            raise serializers.ValidationError("Invalid facility IDs. IDs must be integers.")
+
+    def get_queryset(self):
+        """Retrieve the shelters with filters"""
+        capacity = self.request.query_params.get("capacity")
+        facilities = self.request.query_params.get("facilities")
+        shelter_type = self.request.query_params.get("shelter_type")
+
+        queryset = self.queryset
+
+        if capacity:
+            capacity = int(capacity)
+            queryset = queryset.filter(capacity__gte=capacity)
+
+        if facilities:
+            facilities_ids = self._params_to_ints(facilities)
+            queryset = queryset.filter(facilities__id__in=facilities_ids)
+
+        if shelter_type:
+            queryset = queryset.filter(shelter_type=shelter_type)
+
+        return queryset.distinct()
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "capacity",
+                type=OpenApiTypes.NUMBER,
+                description="Filter by shelter capacity (ex. ?capacity=5)",
+            ),
+            OpenApiParameter(
+                "facilities",
+                type={"type": "list", "items": {"type": "number"}},
+                description="Filter by facilities id (ex. ?facilities=1,2)",
+            ),
+            OpenApiParameter(
+                "shelter_type",
+                type=OpenApiTypes.NUMBER,
+                description="Filter by shelter type id (ex. ?shelter_type=1)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
 
 class ShelterTypeCreateView(generics.CreateAPIView):
