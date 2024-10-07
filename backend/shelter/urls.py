004e70a8554f263e4c3a50bf021cd618ae5feb8d
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2024-10-07 22:40:44 +0300

    connected static

--- backend/shelter/urls.py
+++ backend/shelter/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework.routers import DefaultRouter
 
@@ -16,4 +18,4 @@ urlpatterns = [
     path("", include(router.urls)),
     path("shelter-types/", ShelterTypeCreateView.as_view(), name="create-shelter-type"),
     path("facilities/", FacilityCreateView.as_view(), name="create-facility"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- backend/shelter_service/settings.py
+++ backend/shelter_service/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-j35j4t^83s$kl^dj36(jjq$ge5-($5s8#-kly-g=dw75r&rim3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
